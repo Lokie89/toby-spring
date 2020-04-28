6324c33a6d15559f588784ed4674dde0124be975
Lokie89 <traeuman@gmail.com>
2020-04-28 15:12:15 +0900

    2-2 UserDaoTest 개선

--- src/test/java/dao/UserDaoTest.java
+++ src/test/java/dao/UserDaoTest.java
@@ -1,15 +1,22 @@
 package dao;
 
 import domain.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.sql.SQLException;
 
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.*;
+
+@RunWith(JUnit4.class)
 public class UserDaoTest {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+    @Test
+    public void addAndGet() throws SQLException {
         ApplicationContext context = new GenericXmlApplicationContext("dao/applicationContext.xml");
         UserDao dao = context.getBean("userDao", UserDao.class);
         User user = new User();
@@ -21,9 +28,9 @@ public class UserDaoTest {
         System.out.println(user.getId() + " 등록 성공");
 
         User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
 
-        System.out.println(user2.getId() + " 조회 성공");
+        assertThat(user2.getName(),is(user.getName()));
+        assertThat(user2.getPassword(),is(user.getPassword()));
     }
+
 }
